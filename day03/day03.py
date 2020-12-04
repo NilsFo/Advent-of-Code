349029e01881835d55aa317caef282e84b03f2bd
NilsFo <nils.foerster@yahoo.de>
2020-12-04 16:35:01 +0100

    Day 03, extended

--- day03/day03.py
+++ day03/day03.py
@@ -1,11 +1,10 @@
 def main():
+    #f = open('example_input.txt')
     f = open('input.txt')
     lines = f.readlines()
     f.close()
 
-    x = 0
-    y = 0
-
+    tree_count = 0
     tree_count = check_slope(lines, 1, 1)
     tree_count = tree_count * check_slope(lines, 3, 1)
     tree_count = tree_count * check_slope(lines, 5, 1)
@@ -15,24 +14,23 @@ def main():
     print('Final tree count: ' + str(tree_count))
 
 
-def check_slope(lines, x: int = 0, y: int = 0):
+def check_slope(lines, x_step: int = 0, y_step: int = 0):
     tree_count = 0
-    start_x = x
-    start_y = y
+    x = x_step
+    y = y_step
 
     while y < len(lines):
         line = lines[y].strip()
         c = line[x % len(line)]
 
         is_tree = c == '#'
-
-        y = y + 1
-        x = x + 3
-
         if is_tree:
             tree_count = tree_count + 1
 
-    print('x=' + str(start_x) + ' y=' + str(start_y) + '. Trees encountered: ' + str(tree_count))
+        y = y + y_step
+        x = x + x_step
+
+    print('x=' + str(x_step) + ' y=' + str(y_step) + '. Trees encountered: ' + str(tree_count))
     return tree_count
 
 
